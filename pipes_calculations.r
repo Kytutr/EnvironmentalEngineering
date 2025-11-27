bc12f8fa8f375277630d5dd1ab313a98919d0910
Marek Pałach-Rydzy <C:\Users\marek\Mail>
2025-11-27 22:30:01 +0100

    Adding more output and adding license

--- pipes_calculations.r
+++ pipes_calculations.r
@@ -1,3 +1,23 @@
+# MIT License
+# Copyright (c) 2025 Marek Pałach-Rydzy
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 library(optparse)
 
 # Parsujemy argumenty z linii poleceń, w zasadzie musimy podać jedynie Q
@@ -144,7 +164,9 @@ deltahTM <- deltah(lTM, D, u, g)
 
 print(paste("Po wykonaniu obliczeń dla Q=", Q, "[dm3*s-1]"))
 print(paste("Preferowana średnica rury:", prefferedPipe$PipeNumber))
-print(paste("Szybkość wody w rurze:", u, "[m*s-1]"))
+print(paste("Szybkość wody w rurze u=", u, "[m*s-1]"))
+print(paste("Liniowy spadek ciśnienia I=", deltahTM, "[‰]"))
+
 
 if (verbose) {
     print(paste("Chropowatośc bezwzględna:", k, "[m]"))
